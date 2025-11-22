6a6635b8f7218f262ae3edb5ce3c3e788d24ee82
minty <eithanmathewmalonzo@gmail.com>
2025-11-22 11:07:32 +0800

    Used gradle build tool for the project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "CC4-Case-Study"
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
+package org.example;
+
 import java.util.Scanner;
 
-public class Main {
+public class Macin {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
